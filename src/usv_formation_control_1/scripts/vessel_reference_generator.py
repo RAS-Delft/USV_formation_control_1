f2f5510dff3a009208645b6d80da2c5e4f84190a
Bart <bartboogmans@hotmail.com>
2023-04-20 14:59:25 +0200

    initial setup of vessel reference generator

---
+++ src/usv_formation_control_1/scripts/vessel_reference_generator.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+"""
+Generates a desired state (target pose and/or velocities) of a vessel based on desired state within a fleet formation. 
+Initial developer: Bart Boogmans (bartboogmans@hotmail.com)
+
+"""
