6c990af2a625f0362defebc0dd59bac6cffeefa9
Bart <bartboogmans@hotmail.com>
2023-12-07 11:32:23 +0100

    create formation control launch system

---
+++ launch/formation1.launch.py
@@ -0,0 +1,79 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import ras_ros_core_control_modules.tools.display_tools as ras_display_tools
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Define the vesselids that are used in this formation control stack
+    vesselids = ['RAS_TN_DB','RAS_TN_OR','RAS_TN_GR']
+
+    # Bring up the simulation fleet stack
+    vesselnodes = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            get_package_share_directory('ras_ros_core_control_modules'),
+            '/fleet_sim.launch.py'])
+        )
+    ld.add_action(vesselnodes)
+
+    # Bring up the control stack for each vessel
+    for vesselid in vesselids:
+        # Start vessel reference generator for each vessel
+        ref_generator_node = Node(
+            package='usv_formation_control_1',
+            executable='vessel_reference_generator',
+            name='vessel_reference_generator',
+            namespace=vesselid,
+            arguments=[vesselid,'1.8'],
+            output='screen',
+            emulate_tty=True,
+        )
+        ld.add_action(ref_generator_node)
+
+        # Start vessel heading controller for each vessel
+        heading_controller_node = Node(
+            package=['ras_ros_core_control_modules'],
+            executable='heading_controller',
+            name='heading_controller',
+            namespace=vesselid,
+            arguments=[vesselid],
+            output='screen',
+            emulate_tty=True,
+        )
+        ld.add_action(heading_controller_node)
+
+        # Start vessel surge velocity controller for each vessel
+        surge_controller_node = Node(
+            package=['ras_ros_core_control_modules'],
+            executable='surge_velocity_controller',
+            name='surge_velocity_controller',
+            namespace=vesselid,
+            arguments=[vesselid],
+            output='screen',
+            emulate_tty=True,
+        )
+        ld.add_action(surge_controller_node)
+        
+        # Start thrust allocator for each vessel
+        thrust_allocator_node = Node(
+            package=['ras_ros_core_control_modules'],
+            executable='TN_control_effort_allocator_nomoto',
+            name='thrust_allocator',
+            namespace=vesselid,
+            arguments=[vesselid],
+            output='screen',
+            emulate_tty=True,
+        )
+        ld.add_action(thrust_allocator_node)
+
+    # Start formation configuration broadcaster
+
+    # start formation trajectory planner
+
+    # Start Rviz2
+
+
+    return ld
