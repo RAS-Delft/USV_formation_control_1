a29e521c703fad21b7da1ac2829ed368e1374861
Bart <bartboogmans@hotmail.com>
2023-12-13 10:52:58 +0100

    Add files and entry points to build

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'usv_formation_control_1'
 
@@ -7,9 +9,17 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        # import marker file and package.xml
+        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+
+        # import python package
+        (os.path.join('lib', package_name), glob(os.path.join(package_name, '*.py'))),
+
+        # import all launch files
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +30,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'vessel_reference_generator = '+package_name+'.vessel_reference_generator:main',
+            'formation_trajectory_planner = '+package_name+'.formation_trajectory_planner:main',
+            'formation_configuration_broadcaster_1 = '+package_name+'.formation_configuration_broadcaster_1:main',
+            
         ],
     },
 )
